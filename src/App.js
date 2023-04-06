159e3c858259807f9037a0ba1f12850c39bc21d3
TosinMary <teetoastl3@gmail.com>
2023-04-06 16:06:08 +0100

    lightbox removal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navigation from '../src/component/navigation/Navigation';
 import Display from './component/display/Display';
-import Lightbox from './Lightbox';
+
 
 
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <Lightbox />
+      
       <Display />
      
     </div>
